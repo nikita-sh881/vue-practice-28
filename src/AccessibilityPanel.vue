c05e6443852c329ef596bb433bebb0ebee40b557
nikita-sh881 <olegotvalov99@gmail.com>
2025-12-15 20:30:21 +0300

    Create AccessibilityPanel.vue

---
+++ src/AccessibilityPanel.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="accessibility-panel">
+    <h3>Анализ доступности</h3>
+    
+    <div v-if="accessibilityResults.length" class="accessibility-results">
+      <h4>Контрастность цветов:</h4>
+      
+      <div class="contrast-grid">
+        <div 
+          v-for="result in accessibilityResults" 
+          :key="`${result.color1}-${result.color2}`"
+          class="contrast-item"
+          :style="{ borderLeftColor: result.color }"
+        >
+          <div class="color-pair">
+            <div 
+              class="color-sample" 
+              :style="{ backgroundColor: result.color1 }"
+            ></div>
+            <div class="vs">vs</div>
+            <div 
+              class="color-sample" 
+              :style="{ backgroundColor: result.color2 }"
+            ></div>
+          </div>
+          
+          <div class="contrast-info">
+            <div class="contrast-ratio">
+              <strong>Контраст:</strong> {{ result.
